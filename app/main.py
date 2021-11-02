6ab26446328406f0900b24c7740a752af04fb856
jjackowski47 <49172089+jjackowski47@users.noreply.github.com>
2021-11-02 23:20:48 +0100

    Add cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from app.api.forestry_api import forestry_api
 from app.api.sensors_api import sensor_api
 
@@ -6,6 +7,7 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.register_blueprint(forestry_api)
 app.register_blueprint(sensor_api)
+CORS(app)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
